878a14097f3c1f915fe332f8f1cf673266c1538c
josephxaviour <josephxaviour007@gmail.com>
2022-11-18 00:46:25 +0530

    Day3_problems

---
+++ src/PRINT/ascending.java
@@ -0,0 +1,27 @@
+package PRINT;
+
+class GFG {
+    static int length;
+    public static void printArray(int[] array)
+    {
+        for (int i = 0; i < length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void sortArray(int[] array)
+    {
+        int temporary = 0;
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j < length; j++) {
+                if (array[i] > array[j]) {
+                    temporary = array[i];
+                    array[i] = array[j];
+                    array[j] = temporary;
+                }
+            }
+        }
+        System.out.println("Elements of array sorted in ascending order: ");
+        printArray(array);
+    }
---
+++ src/PRINT/printele.java
@@ -0,0 +1,15 @@
+package PRINT;
+
+class A
+{
+    public static void main (string[] args)
+    {int a[]=new int[3];
+        a[0]=10;
+        a[1]=20;
+        a[2]=30;
+        for(int b : a)
+        {
+            system.out.print(b+"");
+        }
+    }
+}
